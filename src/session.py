1895221e08ffedaef113bb74dda09d5033e5262a
DragonWF <plarisanmarc@gmail.com>
2022-03-17 01:05:07 +0800

    Added main class attributes

--- src/session.py
+++ src/session.py
@@ -1,3 +1,12 @@
+import json
+from utils import Utilities
+
+
 class Session:
     def __init__(self):
+        self.race_counts = 0
+        self.universe = "Main"  # Default for now
+        self.result = None  # Can either be "Success" or "Error"
+
+    def save_session(self):
         pass
