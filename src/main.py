d40414d8ff98074b1629307528a8a58ac6e6764f
DragonWF <plarisanmarc@gmail.com>
2022-03-17 01:20:06 +0800

    Added logic for configure_bot()

--- src/main.py
+++ src/main.py
@@ -5,15 +5,24 @@ from session import Session
 from utils import Utilities
 
 session = Session()
-bot = Bot(config.username, config.password, session, 25)
 
 
 def configure_bot():
-    pass
+    try:
+        while True:
+            races = int(input("$ How many races would you like to run: "))
+            settings = {"race_runs": races}
+            break
+    except ValueError:
+        Utilities.colored_print("Invalid Input", color="red")
+
+    return settings
 
 
 def main():
-    configure_bot()
+    configuration = configure_bot()
+    bot = Bot(config.username, config.password,
+              session, configuration["race_runs"])
     Utilities.text_to_speech("Bot is now running!")
     bot.run()
 
