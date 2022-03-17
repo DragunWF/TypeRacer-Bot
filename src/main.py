e984d92bdcd30e9b2da102d17102edd65d1d1755
DragonWF <plarisanmarc@gmail.com>
2022-03-17 13:22:57 +0800

    Added logic for configuration

--- src/main.py
+++ src/main.py
@@ -3,16 +3,28 @@ import config
 from bot import Bot
 from session import Session
 from utils import Utilities
+from colored import fg
 
 session = Session()
 
 
 def configure_bot():
+    cyan, white = fg("light_cyan"), fg("white")
     try:
         while True:
-            races = int(input("$ How many races would you like to run: "))
-            settings = {"race_runs": races}
+            default = input(
+                cyan + "Would you like to use the default settings? (y/n) " + white).strip().lower()
+            if default == "y" or default == "yes":
+                races = 100
+                universe = "play"
+            elif default == "n" or default == "no":
+                races = int(
+                    input(cyan + "$ How many races would you like to run: " + white))
+                universe = input(cyan + "$ Universe: " + white).strip().lower()
+            else:
+                Utilities.colored_print("Invalid option!", color="red")
             break
+        settings = {"race_runs": races, "universe": universe}
     except ValueError:
         Utilities.colored_print("Invalid Input", color="red")
 
@@ -22,8 +34,9 @@ def configure_bot():
 def main():
     configuration = configure_bot()
     bot = Bot(config.username, config.password,
-              session, configuration["race_runs"])
-    Utilities.text_to_speech("Bot is now running!")
+              session, configuration["race_runs"],
+              configuration["universe"])
+    Utilities.tts_print("Bot is now running!", color="green")
     bot.run()
 
 
