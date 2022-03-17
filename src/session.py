6a186e6001697038ac19ff8ebede4faf80d83b10
DragonWF <plarisanmarc@gmail.com>
2022-03-17 17:57:39 +0800

    Added main variables

--- src/session.py
+++ src/session.py
@@ -5,8 +5,10 @@ from utils import Utilities
 class Session:
     def __init__(self):
         self.race_counts = 0
-        self.universe = "Main"  # Default for now
+        self.wins = 0
+        self.losses = 0
+        self.universe = None
         self.result = None  # Can either be "Success" or "Error"
 
     def save_session(self):
-        pass
+        file_location = "data/sessions.json"
