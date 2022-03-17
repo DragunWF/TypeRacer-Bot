0ce5d1dac804c779fac1aaebf8f45530123610f2
DragonWF <plarisanmarc@gmail.com>
2022-03-17 20:45:39 +0800

    Added function that saves your session (stats)

--- src/session.py
+++ src/session.py
@@ -1,14 +1,23 @@
 import json
-from utils import Utilities
+from pathlib import Path
 
 
 class Session:
     def __init__(self):
-        self.race_counts = 0
+        self.races = 0
         self.wins = 0
         self.losses = 0
         self.universe = None
-        self.result = None  # Can either be "Success" or "Error"
+        self.result = None
 
     def save_session(self):
-        file_location = "data/sessions.json"
+        result = "success" if self.result else "interrupted"
+        data = json.loads(Path("data/sessions.json").read_text())
+        session_data = {"races": self.races,
+                        "wins": self.wins, "losses": self.losses,
+                        "universe": self.universe, "session_result": result}
+        data.append(session_data)
+
+        formatted = json.dumps(data, sort_keys=False,
+                               indent=2, separators=(',', ': '))
+        Path("data/sessions.json").write_text(formatted)
