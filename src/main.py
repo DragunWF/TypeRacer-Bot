c5204416a8eb6b15e96032b24b4dd97523801560
DragonWF <plarisanmarc@gmail.com>
2022-03-17 20:45:11 +0800

    Default settings now grabs its properties from settings.json

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import json
 import config
+from pathlib import Path
+from colored import fg
+
 from bot import Bot
 from session import Session
 from utils import Utilities
-from colored import fg
 
 session = Session()
 cyan, white = fg("light_cyan"), fg("white")
@@ -12,25 +14,27 @@ cyan, white = fg("light_cyan"), fg("white")
 def change_settings():
     while True:
         try:
-            races = int(input(cyan + "$ How many races would you like to run: "
+            races = int(input(cyan + "How many races would you like to run: "
                               + white))
-            universe = input(cyan + "$ Universe: " +
+            universe = input(cyan + "Universe: " +
                              white).strip().lower()
             break
         except ValueError:
-            Utilities.colored_print("Make input an integer value...",
+            Utilities.colored_print("Make sure your input is an integer value...",
                                     color="red")
 
     return races, universe
 
 
 def configure_bot():
+    default_settings = json.loads(Path("data/settings.json").read_text())[0]
+
     while True:
         default = input(cyan + "Would you like to use the default settings? (y/n) "
                         + white).strip().lower()
         if default == "y" or default == "yes":
-            races = 100
-            universe = "play"
+            races = default_settings["races"]
+            universe = default_settings["universe"]
             break
         elif default == "n" or default == "no":
             races, universe = change_settings()
