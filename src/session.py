4c9524182a3723d64c33e75a9dbc4a9e1299fdbe
DragonWF <plarisanmarc@gmail.com>
2022-03-21 02:42:30 +0800

    Added information validation for class instance variables

--- src/session.py
+++ src/session.py
@@ -9,12 +9,21 @@ class Session:
         self.losses = 0
         self.universe = None
         self.result = None
+        self.registered = None
+        self.practice_mode = None
+
+    def validate_information(self):
+        if self.practice_mode:
+            self.wins = None
+            self.losses = None
 
     def save_session(self):
+        self.validate_information()
+
         result = "success" if self.result else "interrupted"
         data = json.loads(Path("data/sessions.json").read_text())
-        session_data = {"races": self.races,
-                        "wins": self.wins, "losses": self.losses,
+        session_data = {"registered": self.registered, "practice_mode": self.practice_mode,
+                        "races": self.races, "wins": self.wins, "losses": self.losses,
                         "universe": self.universe, "session_result": result}
         data.append(session_data)
 
