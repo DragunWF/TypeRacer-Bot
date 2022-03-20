a4089c652c1538cc83e9002c0472270ca02a6095
DragonWF <plarisanmarc@gmail.com>
2022-03-21 02:42:04 +0800

    Optimized code and added more config options

--- src/main.py
+++ src/main.py
@@ -8,48 +8,51 @@ from session import Session
 from utils import Utilities
 
 session = Session()
-cyan, white = fg("light_cyan"), fg("white")
+cyan, white, red = fg("light_cyan"), fg("white"), fg("light_red")
 
 
-def change_settings():
+def user_choose(question) -> bool:
+    option = input(cyan + f"{question} (y/n) " + white).strip().lower()
+    if option == "y" or option == "yes":
+        return True
+    if option == "n" or option == "no":
+        return False
+    Utilities.colored_print("Invalid input!", color="red")
+    user_choose(question)
+
+
+def change_settings() -> tuple:
     while True:
         try:
-            races = int(input(cyan + "How many races would you like to run: "
-                              + white))
-            universe = input(cyan + "Universe: " +
-                             white).strip().lower()
+            races = int(input(cyan + "How many races would you like to run: "+ white))
+            universe = input(cyan + "Universe: " + white).strip().lower()
+            registered = user_choose("Are you going to play in a registered account?")
+            practice_mode = user_choose("Are you going to play in practice mode?")
             break
         except ValueError:
             Utilities.colored_print("Make sure your input is an integer value...",
                                     color="red")
 
-    return (races, universe)
+    return (races, universe, registered, practice_mode)
 
 
-def configure_bot():
+def configure_bot() -> dict:
     default_settings = json.loads(Path("data/settings.json").read_text())[0]
+    use_default = user_choose("Would you like to use the default settings?")
 
-    while True:
-        default = input(cyan + "Would you like to use the default settings? (y/n) "
-                        + white).strip().lower()
-        if default == "y" or default == "yes":
-            break
-        elif default == "n" or default == "no":
-            changes = change_settings()
-            default_settings["races"] = changes[0]
-            default_settings["universe"] = changes[1]
-            break
-        else:
-            Utilities.colored_print("Invalid option!", color="red")
+    if not use_default:
+        changes = change_settings()
+        default_settings["races"] = changes[0]
+        default_settings["universe"] = changes[1]
+        default_settings["registered"] = changes[2]
+        default_settings["practice_mode"] = changes[3]
 
     return default_settings
 
 
 def main():
     settings = configure_bot()
-    bot = Bot(config.username, config.password,
-              session, settings["races"],
-              settings["universe"], settings["key_intervals"])
+    bot = Bot(config.username, config.password, session, settings)
     Utilities.tts_print("Bot is now running!", color="green")
     bot.run()
 
